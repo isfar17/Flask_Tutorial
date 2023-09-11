b31be468a9acdb5268bf07f350be7f4ce354c911
isfar17 <isfarjubayer@gmail.com>
2023-09-12 01:56:50 +0600

    adding pics and code change

--- 06.Flask Admin Advanced/2.flask_Admin( blueprint mulpi app/main.py
+++ 06.Flask Admin Advanced/2.flask_Admin( blueprint mulpi app/main.py
@@ -1,9 +1,6 @@
 from myproject import app
 from flask import render_template
 
-@app.route("/")
-def index():
-    return render_template("index.html")
 
 if __name__=="__main__":
     app.run(debug=True)
